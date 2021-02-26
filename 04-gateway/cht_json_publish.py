b16029c834096b84ff0f19a982ba2858c27f5df4
Lunatic Luna <59903692+iishiishii@users.noreply.github.com>
2021-02-26 14:29:53 +0800

    mqtt connect in lora.start

--- 04-gateway/cht_json_publish.py
+++ 04-gateway/cht_json_publish.py
@@ -121,6 +121,22 @@ class LoRaRcvCont(LoRa):
             sys.stdout.flush()
             sys.stdout.write("\r%d %d %d" % (rssi_value, status['rx_ongoing'], status['modem_clear']))
 
+            returnedList = lora.on_rx_done()
+            print("payload")
+            client = mqtt.Client()
+            
+            # for tago include this line
+            client.username_pw_set(mqtt_username, mqtt_password)
+            
+            client.on_connect = on_connect
+            client.connect(broker, broker_port, mqtt_keep_alive)
+            client.loop_start()
+            
+            data_json_string = json.dumps(returnedList)
+            
+            # 20 device every 2s
+            time.sleep(2)
+
 
 
 # publish to TagoIO
@@ -156,21 +172,7 @@ except: pass
 try:
     lora.start()
     #while True:
-    returnedList = lora.on_rx_done()
-    print("payload")
-    client = mqtt.Client()
     
-    # for tago include this line
-    client.username_pw_set(mqtt_username, mqtt_password)
-    
-    client.on_connect = on_connect
-    client.connect(broker, broker_port, mqtt_keep_alive)
-    client.loop_start()
-    
-    data_json_string = json.dumps(returnedList)
-    
-    # 20 device every 2s
-    time.sleep(2)
 except KeyboardInterrupt:
     sys.stdout.flush()
     sys.stderr.write("KeyboardInterrupt\n")
